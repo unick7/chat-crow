9b773f8ab059c61fed269852cf0821299c597c92
Nikhil Verma <lci2020044@iiitl.ac.in>
2023-07-01 16:10:15 +0530

    initial setup

---
+++ src/component/Join/Join.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Join.css";
+import logo from "../../Image/chat.png";
+
+const Join = () => {
+  return (
+    <div className="JoinPage">
+      <div className="JoinContainer">
+        <img src={logo} alt="logo" />
+        <h1>Join Page</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
